7efffb7b751f3bb7b716cc63d4b3d6de16ba9ed5
Chia Chin Tat <chiachintat10@gmail.com>
2025-04-07 16:42:27 +0800

    Update 3Classes.py

--- 3Classes.py
+++ 3Classes.py
@@ -103,8 +103,7 @@ transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.CenterCrop(224),
     transforms.ToTensor(),
-    transforms.Normalize([0.5, 0.5, 0.5],
-                         [0.5, 0.5, 0.5])
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 
 # ---------------- Video Functions ----------------
